b57c792c38db03a336a3b8b68e5a053109c28b7a
Andrew <35283726+switch900@users.noreply.github.com>
2018-09-29 12:29:40 -0700

    Add files via upload

--- Assignment01/Employee.h
+++ Assignment01/Employee.h
@@ -9,22 +9,20 @@
 #pragma once
 #include <string>
 
-using namespace std;
-
 //class Employee definition
 class Employee
 {
 public:
-	explicit Employee(string, string, int);//constructor initializes data members
+	explicit Employee(std::string, std::string, int);//constructor initializes data members
 	string getFirstName() const;//return first name as string
 	string getLastName() const;//return last name as string
 	int getMonthlySalary() const;//return monthly salary as int
-	void setFirstName(string);//set first name
-	void setLastName(string);//set last name
+	void setFirstName(std::string);//set first name
+	void setLastName(std::string);//set last name
 	void setMonthlySalary(int);//set monthly salary
 
 private:
-	string firstName;//the employee's first name
-	string lastName;//the employee's last name
+	std::string firstName;//the employee's first name
+	std::string lastName;//the employee's last name
 	int monthlySalary;//the employee's monthly salary
 };//end class Employee
